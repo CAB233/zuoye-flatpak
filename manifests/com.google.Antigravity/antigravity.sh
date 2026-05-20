4c00212fa53e6223a0f5632d6cd88b8e32cb5a86
CAB233 <yidaduizuoye@outlook.com>
2026-05-20 10:46:57 +0800

    com.google.Antigravity: drop

--- manifests/com.google.Antigravity/antigravity.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ELECTRON_RUN_AS_NODE=1 \
-    exec \
-    /app/bin/zypak-wrapper.sh \
-    /app/extra/antigravity/antigravity \
-    /app/extra/antigravity/resources/app/out/cli.js "$@"
